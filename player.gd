1e5f0303b3bd8c352ee8d2bcd4e3c3814ca348da
qujimin <rafsil36@kingshigh.school.nz>
2024-07-01 14:57:49 +1200

    vers 3

--- player.gd
+++ player.gd
@@ -10,8 +10,42 @@ enum state {IDLE, RUNNING, JUMPUP, JUMPDOWN, HURT}
 
 var anim_state = state.IDLE
 
+@onready var animator = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func update_state():
+	if anim_state == state.HURT:
+		return
+	if is_on_floor():
+		if velocity == Vector2.ZERO:
+			anim_state = state.IDLE
+		elif velocity.x != 0:
+			anim_state = state.RUNNING
+	else:
+		if velocity.y < 0:
+			anim_state = state.JUMPUP
+		else:
+			anim_state = state.JUMPDOWN
+
+func update_animation(direction):
+	if direction > 0:
+		animator.flip_h = false
+	elif direction < 0:
+		animator.flip_h = true
+	match anim_state:
+		state.IDLE:
+			animation_player.play("idle")
+		state.RUNNING:
+			animation_player.play("run")
+		state.JUMPUP:
+			animation_player.play("jump_up")
+		state.JUMPDOWN:
+			animation_player.play("jump_down")
+		state.HURT:
+			animation_player.play("hurt")
 
 
 func _physics_process(delta):
@@ -29,6 +63,8 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = move_toward(velocity.x, speed,direction * accelartion)
 	else:
-		velocity.x = move_toward(velocity.x, 0, accelartion/2)
-
+		velocity.x = move_toward(velocity.x, 0, accelartion)
+		
+	update_state()
+	update_animation(direction)
 	move_and_slide()
