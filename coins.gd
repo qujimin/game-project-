953d4fb71f7284e349a55c5bb19a1f28826fd817
qujimin <rafsil36@kingshigh.school.nz>
2024-08-13 11:01:26 +1200

    vers11
    significant progress towards my collectable items hehehe

---
+++ coins.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func collected():
+	pass
+	
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		collected()
+		queue_free()
