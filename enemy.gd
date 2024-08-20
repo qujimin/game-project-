c12477d759d21a042989e42fe38ea92218817bff
qujimin <rafsil36@kingshigh.school.nz>
2024-08-21 10:23:39 +1200

    vers13.2
    yep clock

---
+++ enemy.gd
@@ -0,0 +1,16 @@
+extends Area2D
+@export var flip_time = 1
+var direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Timer.wait_time = flip_time
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	translate(Vector2.RIGHT * direction)
+	$AnimatedSprite2D.flip_h = direction > 0
+	
+
+func _on_timer_timeout() -> void:
+	direction *= -1
