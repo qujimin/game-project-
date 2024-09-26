fc871e0f94fb7dbbad8c7a91a4b60fc992adebdc
qujimin <rafsil36@kingshigh.school.nz>
2024-09-27 09:33:15 +1200

    Update speedrun.gd

--- speedrun.gd
+++ speedrun.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
- 
+
 var time = Global.speedrun_time
- 
+
 func _physics_process(delta):
 	time = float(time) + delta
 	update_ui()
-	
+	# Updates the time variable by adding delta to the current time.
+
 func update_ui():
 	# Format time with two decimal places
 	var formatted_time = str(time)
@@ -13,9 +14,7 @@ func update_ui():
 	
 	if decimal_index > 0:
 		formatted_time = formatted_time.left(decimal_index + 3)  # Take only two decimal places
-	 
+	
 	Global.speedrun_time = formatted_time
-
-		
 	$Label.text = formatted_time
- 
+	# Formats the time to two decimal places and updates the global variable and the UI label.
