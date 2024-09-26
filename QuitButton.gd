267099150d89f29301de4fd9a0db8e2b7c3cacc3
qujimin <rafsil36@kingshigh.school.nz>
2024-09-27 06:56:12 +1200

    Update QuitButton.gd

--- QuitButton.gd
+++ QuitButton.gd
@@ -1,18 +1,15 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-	pass # Replace with function body.
+    pass # Replace with function body.
+# Function to initialize the button or set up any necessary properties when the node is ready.
 
 func _pressed():
-	get_tree().quit()
-
-
-
+    get_tree().quit()
+# Quits the game when the button is pressed.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+    pass
+# Function that is called every frame, can be used to update button behavior if needed.
