d2237a98c7c179a48ac08630b2db29c3f89048c2
qujimin <rafsil36@kingshigh.school.nz>
2024-09-27 09:16:23 +1200

    Update menu.gd

--- menu.gd
+++ menu.gd
@@ -1,17 +1,16 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass # Initial setup when the button node is added to the scene.
 
 func _pressed():
 	get_tree().change_scene_to_file("res://menu.tscn")
+# Changes the current scene to the menu scene when the button is pressed.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	pass # This function is called every frame but currently has no behavior.
 
 func _on_button_2_pressed() -> void:
-	pass # Replace with function body.
+	pass # Placeholder function for an additional button press event.
