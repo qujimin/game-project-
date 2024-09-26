138ad55f0bbaddae7e5acac115b781614556bf46
qujimin <rafsil36@kingshigh.school.nz>
2024-09-27 06:55:36 +1200

    Update PlayButton.gd

--- PlayButton.gd
+++ PlayButton.gd
@@ -1,17 +1,20 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+    pass # Replace with function body.
+# Function to initialize the button or set up any necessary properties when the node is ready.
 
 func _pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+    get_tree().change_scene_to_file("res://world.tscn")
+# Changes the current scene to "world.tscn" when the button is pressed.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+    pass
+# Function that is called every frame, can be used to update button behavior if needed.
 
 func _on_button_2_pressed() -> void:
-	pass # Replace with function body.
+    pass # Replace with function body.
+# Placeholder function for handling a specific button press action; functionality needs to be implemented.
+
