01e8b648d06b29b1f82c0009f30c9be2975509ae
qujimin <rafsil36@kingshigh.school.nz>
2024-09-27 06:59:00 +1200

    Update coins.gd

--- coins.gd
+++ coins.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
+# Called when the item is collected.
 func collected():
-	pass
-	
-	GameManager.score += 1
-
+    pass
+    GameManager.score += 1
+# Increments the player's score by 1 when the item is collected.
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		collected()
-		queue_free()
+    if body.is_in_group("Player"):
+        collected()
+        queue_free()
+# Detects when a body enters the area; if it's the player, collects the item and removes it from the scene.
