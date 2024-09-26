0484e29a9cf635bd0e3d533ad37d35623f0ce1a2
qujimin <rafsil36@kingshigh.school.nz>
2024-09-27 06:55:03 +1200

    Update Label.gd

--- Label.gd
+++ Label.gd
@@ -1,11 +1,11 @@
 extends Label
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+    pass # Replace with function body.
+# Function to initialize any properties or settings when the node is ready.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	text = "SCORE: " + str(GameManager.score)
+    text = "SCORE: " + str(GameManager.score)
+# Updates the label text to display the current score from GameManager every frame.
