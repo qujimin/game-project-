5299b5698e0cf580552a56898df25e1c38f81be7
qujimin <rafsil36@kingshigh.school.nz>
2024-09-27 06:57:18 +1200

    Update button_2.gd

--- button_2.gd
+++ button_2.gd
@@ -1,11 +1,11 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+    pass # Replace with function body.
+# Function to initialize the button or set up any necessary properties when the node is ready.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+    pass
+# Function that is called every frame, can be used to update button behavior if needed.
